b8b01ec5b8b9bfbb73def403c3416a3b34f296bb
Elad <eladnah@gmail.com>
2019-12-31 18:36:41 +0200

    fix setup.py package info file (dependency & modules)

--- setup.py
+++ setup.py
@@ -1,12 +1,21 @@
+#!/usr/bin/env
+
 import setuptools
+
+"""
+>>> python setup.py bdist_wheel
+>>> python -m pip install dist/confclass-<XX.XX>-py3-none-any.whl
+>>> python -m twine upload dist/*
+"""
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
     name='confclass',
-    version='0.1',
-    scripts=['confclass.py'],
+    version='0.1.2',
+    provides=['confclass'],
+    py_modules=['confclass'],
     author="Elad Nachmias",
     author_email="eladnah@gmail.com",
     description="A python package for working with program configuration parameters",
@@ -14,7 +23,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/eladn/confclass",
     packages=setuptools.find_packages(),
-    install_requires=['yaml'],
+    install_requires=['pyyaml'],
     license='Apache-2.0',
     classifiers=[
         "Programming Language :: Python :: 3",
