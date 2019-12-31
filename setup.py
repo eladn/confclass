203988950dc3c80ded59f22ea213755bae61698c
Elad <eladnah@gmail.com>
2019-12-31 17:47:27 +0200

    add license & dependency to package setup file

--- setup.py
+++ setup.py
@@ -14,6 +14,8 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/eladn/confclass",
     packages=setuptools.find_packages(),
+    install_requires=['yaml'],
+    license='Apache-2.0',
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
